d6211604200e74dd47de8e5545342574a6260979
isurumanod <isurumanod99@gmail.com>
2023-04-06 06:56:57 +0530

    smtp configuration

--- Hackthon_Registration_Webapp/settings.py
+++ Hackthon_Registration_Webapp/settings.py
@@ -129,3 +129,9 @@ MEDIA_URL = "/images/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 567
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "isurumanoddev@gmail.com"
+EMAIL_HOST_PASSWORD = "uuzumpvzgxojblfy"
