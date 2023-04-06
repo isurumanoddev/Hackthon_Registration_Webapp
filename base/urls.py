c4a47f2412441da1a2646aa51d9c9258017c8bb6
isurumanod <isurumanod99@gmail.com>
2023-04-06 06:55:26 +0530

    password reset urls

--- base/urls.py
+++ base/urls.py
@@ -18,5 +18,8 @@ urlpatterns = [
     path('event_submission/<str:pk>', views.event_submission, name="event-submission"),
     path('update_submission/<str:pk>', views.update_submission, name="update-submission"),
 
-
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
