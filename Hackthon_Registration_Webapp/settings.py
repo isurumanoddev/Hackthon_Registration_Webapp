a449a14eeb8176cbc100023d74fbb6866ab8291c
isurumanod <isurumanod99@gmail.com>
2023-04-08 17:07:35 +0530

    password reset complete
    Signed-off-by: isurumanod <isurumanod99@gmail.com>

--- Hackthon_Registration_Webapp/settings.py
+++ Hackthon_Registration_Webapp/settings.py
@@ -134,4 +134,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "isurumanoddev@gmail.com"
-EMAIL_HOST_PASSWORD = "uuzumpvzgxojblfy"
+EMAIL_HOST_PASSWORD = "kgetxjvzpkkoksye"
--- base/urls.py
+++ base/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     # path('password_reset/', auth_views.PasswordResetView.as_view(template_name=""), name="password_reset"),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
     path('password_reset_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_done.html"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
