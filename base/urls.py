45e555d987394da2bfb81556ec01e0ebb586a426
isurumanod <isurumanod99@gmail.com>
2023-04-08 18:25:12 +0530

    password reset ui update
    Signed-off-by: isurumanod <isurumanod99@gmail.com>

--- base/urls.py
+++ base/urls.py
@@ -18,9 +18,10 @@ urlpatterns = [
     path('event_submission/<str:pk>', views.event_submission, name="event-submission"),
     path('update_submission/<str:pk>', views.update_submission, name="update-submission"),
 
-    # path('password_reset/', auth_views.PasswordResetView.as_view(template_name=""), name="password_reset"),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path('password_reset_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+         name="password_reset"),
+    # path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name="password_reset_complete"),
 ]
